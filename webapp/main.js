a3ca6f78fad0015be179d56d2397f8faab0ffe86
inesbl2 <ines.blancke@gmail.com>
2024-05-28 18:25:07 +0200

    add better counter exo

--- webapp/main.js
+++ webapp/main.js
@@ -3,8 +3,9 @@ let result = document.querySelector("#result")
 counter = 0
 result.innerHTML = counter
 
-function clear(){
-    result.innerHTML = 0;
+function reset(){
+    counter = 0;
+    result.innerHTML = counter;
 }
 
 function plus(){
